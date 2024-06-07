da9753825a17a4c5b42e42ee63532561dcb78a74
anshum12345 <anshumdwivedi8@gmail.com>
2024-06-08 00:50:39 +0530

    Day01

--- Day-01/count-digit.cpp
+++ Day-01/count-digit.cpp
@@ -1,9 +1,27 @@
-// Q) Count Digit(GeeksForGeeks)==============???????????????
+// Question==>> Count Digit(GeeksForGeeks)
 
 // Given a number N. Count the number of digits in N which evenly divide N.
-
 // Note :- Evenly divides means whether N is divisible by a digit i.e. leaves a remainder 0 when divided.
  
+// Example 1:
+// Input:
+// N = 12
+// Output:
+// 2
+// Explanation:
+// 1, 2 both divide 12 evenly
+
+// Example 2:
+// Input:
+// N = 23
+// Output
+// 0
+// Explanation:
+// 2 and 3, none of them
+// divide 23 evenly
+
+// Solution==>>
+
 
 class Solution{
 public:
